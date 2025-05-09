703cbe053723c2bb04e013fc539fe4d20cf6f458
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-05-09 20:07:21 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,17 @@
+# print("\033[4mThis text\033[0m")
+
+def render_menu_principal():
+    print('''
+    -------------------------\n
+    Bem vindo ao Saúde Amiga!\n
+    -------------------------\n
+    
+    ''')
+def main():
+    render_menu_principal()
+
+if(__name__ == '__main__'):
+    main()
+
+
+
