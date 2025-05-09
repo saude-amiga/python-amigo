34b6bd36880ee10b855ef437026058a17c2ca188
BragaSoterasVinicius <viniciussoteras@gmail.com>
2025-05-09 20:23:40 -0300

    Adicionadas variáveis de index de janelas e histórico, assim como index de histório e método de render de página

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 # print("\033[4mThis text\033[0m")
 
-def render_menu_principal():
-    print('''
+TITULO = '''
     -------------------------
     Bem vindo ao Saúde Amiga!
-    -------------------------
-    
-    ''')
+    -------------------------'''
+
+ALTERNATIVAS = {
+    0:["Como faço meu cadastro?",""],
+    1:["Como faço meu login?",""],
+    2:["Como instalo o portal do HC?",""],
+    3:["Como agendo?",""],
+    4:["Como cancelo a consulta?",""],
+    5:["Esqueci minha senha",""],
+    6:["Como reclamar do aplicativo?",""],
+    7:["Como vejo meus documentos?",""],
+    8:["Como abro minha teleconsulta?"],
+    9:["Ver receita"],
+    10:["Ver Resultados"]
+}
+HISTORICO = []
+POSICAO_NO_HISTORICO = 0
+
+def render_menu_principal():
+    print(TITULO)
+    print(ALTERNATIVAS)
+    print(FOOTER)
+
 def main():
     render_menu_principal()
 
